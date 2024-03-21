bed0da549d2cacb325acf84df78ff5141aaa6f3b
FedericaI7 <federicaiuvara3@gmail.com>
2024-03-21 23:28:57 +0100

    add: implement Data.jsx in DoctorCards.jsx and add a slice for 8 results

--- src/components/doctorCards/DoctorCards.jsx
+++ src/components/doctorCards/DoctorCards.jsx
@@ -4,19 +4,20 @@ import Image from "next/image";
 import { userData } from "@/API/Apicall";
 import { useState, useEffect } from "react";
 import { PiPhoneCallFill } from "react-icons/pi";
+import Data from "../data";
 
 const DoctorCards = () => {
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const users = await userData(8);
-      console.log(users);
-      setUser(users);
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const users = await userData(8);
+  //     console.log(users);
+  //     setUser(users);
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   const onHandleCall = (phone) => {
     let phoneNew = phone.replaceAll(" ", "");
@@ -24,35 +25,75 @@ const DoctorCards = () => {
     window.location.href = `tel:${phoneNew}`;
   };
 
-  return (
-    <div className={styles.DoctorCards}>
-      {/* {results?.map((el, index) => {
-        return (
-          <div className={styles.boxCard} key={index}>
-            <div>
-              <Image
-                src={el.picture?.large}
-                width={100}
-                height={100}
-                alt="doctor picture"
-              />
-            </div>
+  // const { results } = user;
 
-            <div className={styles.infoBoxText}>
-              <h3>{`Dr. ${el.name?.first} ${el.name?.last}`}</h3>
-              <p className={styles.city}>{el.location.city}</p>
-              <span
-                onClick={() => onHandleCall(el.phone)}
-                className={styles.telIcon}
-              >
-                <PiPhoneCallFill />
-              </span>
-            </div>
+  return (
+    <Data>
+      {({ user, dataJson }) => {
+        const { results } = user;
+        const firstEightResults = results?.slice(0, 8);
+        return (
+          <div className={styles.DoctorCards}>
+            {firstEightResults?.map((el, index) => {
+              return (
+                <div className={styles.boxCard} key={index}>
+                  <div>
+                    <Image
+                      src={el.picture?.large}
+                      width={100}
+                      height={100}
+                      alt="doctor picture"
+                    />
+                  </div>
+
+                  <div className={styles.infoBoxText}>
+                    <h3>{`Dr. ${el.name?.first} ${el.name?.last}`}</h3>
+                    <p className={styles.city}>{el.location.city}</p>
+                    <span
+                      onClick={() => onHandleCall(el.phone)}
+                      className={styles.telIcon}
+                    >
+                      <PiPhoneCallFill />
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         );
-      })} */}
-    </div>
+      }}
+    </Data>
   );
+
+  // return (
+  //   <div className={styles.DoctorCards}>
+  //     {results?.map((el, index) => {
+  //       return (
+  //         <div className={styles.boxCard} key={index}>
+  //           <div>
+  //             <Image
+  //               src={el.picture?.large}
+  //               width={100}
+  //               height={100}
+  //               alt="doctor picture"
+  //             />
+  //           </div>
+
+  //           <div className={styles.infoBoxText}>
+  //             <h3>{`Dr. ${el.name?.first} ${el.name?.last}`}</h3>
+  //             <p className={styles.city}>{el.location.city}</p>
+  //             <span
+  //               onClick={() => onHandleCall(el.phone)}
+  //               className={styles.telIcon}
+  //             >
+  //               <PiPhoneCallFill />
+  //             </span>
+  //           </div>
+  //         </div>
+  //       );
+  //     })}
+  //   </div>
+  // );
 };
 
 export default DoctorCards;
