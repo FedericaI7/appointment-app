1aa491ad7b8fc9c1131347e5cf82d0c1b578958c
FedericaI7 <federicaiuvara3@gmail.com>
2024-03-23 20:14:56 +0100

    add: appointmentDate in DoctorCards.jsx

--- src/components/doctorCards/DoctorCards.jsx
+++ src/components/doctorCards/DoctorCards.jsx
@@ -7,25 +7,11 @@ import { PiPhoneCallFill } from "react-icons/pi";
 import Data from "../data";
 
 const DoctorCards = () => {
-  // const [user, setUser] = useState({});
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const users = await userData(8);
-  //     console.log(users);
-  //     setUser(users);
-  //   };
-
-  //   fetchData();
-  // }, []);
-
   const onHandleCall = (phone) => {
     let phoneNew = phone.replaceAll(" ", "");
     console.log(phoneNew);
     window.location.href = `tel:${phoneNew}`;
   };
-
-  // const { results } = user;
 
   return (
     <Data>
@@ -49,12 +35,18 @@ const DoctorCards = () => {
                   <div className={styles.infoBoxText}>
                     <h3>{`Dr. ${el.name?.first} ${el.name?.last}`}</h3>
                     <p className={styles.city}>{el.location.city}</p>
-                    <span
-                      onClick={() => onHandleCall(el.phone)}
-                      className={styles.telIcon}
-                    >
-                      <PiPhoneCallFill />
-                    </span>
+
+                    <div className={styles.callToAction}>
+                      <p className={styles.dateAppointment}>
+                        {dataJson[index]?.nextAppointmentDate}
+                      </p>
+                      <span
+                        onClick={() => onHandleCall(el.phone)}
+                        className={styles.telIcon}
+                      >
+                        <PiPhoneCallFill />
+                      </span>
+                    </div>
                   </div>
                 </div>
               );
@@ -64,36 +56,6 @@ const DoctorCards = () => {
       }}
     </Data>
   );
-
-  // return (
-  //   <div className={styles.DoctorCards}>
-  //     {results?.map((el, index) => {
-  //       return (
-  //         <div className={styles.boxCard} key={index}>
-  //           <div>
-  //             <Image
-  //               src={el.picture?.large}
-  //               width={100}
-  //               height={100}
-  //               alt="doctor picture"
-  //             />
-  //           </div>
-
-  //           <div className={styles.infoBoxText}>
-  //             <h3>{`Dr. ${el.name?.first} ${el.name?.last}`}</h3>
-  //             <p className={styles.city}>{el.location.city}</p>
-  //             <span
-  //               onClick={() => onHandleCall(el.phone)}
-  //               className={styles.telIcon}
-  //             >
-  //               <PiPhoneCallFill />
-  //             </span>
-  //           </div>
-  //         </div>
-  //       );
-  //     })}
-  //   </div>
-  // );
 };
 
 export default DoctorCards;
